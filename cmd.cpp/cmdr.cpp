1f78a970f6989db46af0ae26bac95c2bdb9c733a
coder132hello <daanishanwar@icloud.com>
2026-09-21 17:54:17 -0700

    Update cmdr.cpp

--- cmd.cpp/cmdr.cpp
+++ cmd.cpp/cmdr.cpp
@@ -6,12 +6,16 @@ int main() {
   system("title CMDR SPAMMER");
 
 
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
-    cout << "----------------\n";
-    cout << "   C M D R V2    \n";
-    cout << "     SPAMMER     \n";
-    cout << "----------------\n";
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 13);
+    cout << R"(
+   _____ __  __ ____  ____  
+  / ____|  \/  |  _ \|  _ \ 
+ | |    | \  / | | | | |_) |
+ | |____| |\/| | |_| |  _ < 
+  \_____|_|  |_|____/|_| \_\
 
+       S P A M M E R )" << endl;
+    cout << "---------------------------\n";
     cout << "[*] SYSTEM; ONLINE\n";
     cout << "[*] VERSION; V2 BETA\n";
     cout << "[*] STATUS; READY\n";
@@ -29,17 +33,18 @@ int main() {
         Sleep(3000);
 
         for (int i = 0; i < 900000; i++) {
-          SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 13);
           cout << "CMDR SPAMMER #" << i + 1 << "\n";
             cout << "CMDR SPAMMER\n";
             cout << "    V2";
-            cout << "   BETA\n";
+            cout << "   BETA   \n";
         }
       }
 
       else if (choice == 2) {
+        cout << "Trying to make the !next generation spammer!\n";
         cout << "This is a harmless spammer\n";
-        cout << "if you use the spammer you can close it\n";
+        cout << "This doesn't do anything bad\n";
         cout << "Thats it!\n";
       }
 
